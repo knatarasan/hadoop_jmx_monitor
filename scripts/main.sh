59a6a88ac2012054d45bec022e9f67b3aa320985
knatarasan <kannappan_natarasan@yahoo.com>
2019-05-07 15:26:29 -0700

    Working fine, create hourly --> daily --> weekly and make a file for weekly

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,19 +3,22 @@
 log_dir=log
 
 hourly_report_dir=report/hourly
-daily_report_dir=report/daily
+
 db_dir=db
-mkdir -p log report/hourly report/daily db
+mkdir -p log report/weekly report/hourly report/daily db
 poll_id=poll_id_`date +%Y_%m_%d_%H_%M_%S`
 date_id=`date +%Y_%m_%d`
+# date_id=2019_05_06
 month_id=`date +%Y_%m`
 mkdir -p report/hourly/${date_id}
 
 log_file=${log_dir}/app_log_${poll_id}.log
+
 #Send hourly files into folder with name as date
 dn_hourly_report_file=${hourly_report_dir}/${date_id}/dn_report_${poll_id}.csv
-dn_daily_agg_file=${daily_report_dir}/dn_daily_${date_id}.csv
 
+dn_daily_agg_file=report/daily/dn_daily_${date_id}.csv
+dn_weekly_agg_file=report/weekly/dn_weekly_${month_id}.csv
 # dn_hourly_report_file=${hourly_report_dir}/dn_report_poll_id_2019_05_06_16_49_11.csv
 
 echo "main starts here"
@@ -34,6 +37,8 @@ fi
 
 
 prep_dn_hourly_table(){
+  # This hourly table is ddl is derived from metrics list provided from config file
+
   dn_metrics_list=`cat config/dn_metrics_config`
   dn_column_list="HostName poll_id "
 
@@ -48,7 +53,7 @@ prep_dn_hourly_table(){
   dn_hourly_table=dn_hourly
 
   #Prepare DDL of table
-  hourly_tbl_ddl="create table ${dn_hourly_table} ("
+  hourly_tbl_ddl="create table dn_hourly ("
 
   length=${#dn_column_arr[@]}
   current=0
@@ -73,6 +78,9 @@ prep_dn_hourly_table(){
 
 
 poll_hourly_dn_jmx(){
+
+  #Hourly files is prepared by polling all DNs for every 30 min interval, metrics collected would be
+  #stored under report/hourly/yyyy_mm_dd
 
   echo "\nPoll hourly table \n"
   #initialize row values
@@ -100,50 +108,89 @@ poll_hourly_dn_jmx(){
 }
 
 
-load_hourly_table(){
+prep_hourly_file(){
+
+  # read all hourly files under report/hourly/yyyy_mm_dd which was pulled by poll_hourly_dn_jmx
+  # and load into dn_hourly table
 
   echo "\nDDL before create table :\n ${hourly_tbl_ddl}"
   #connect DB create tabel
   sqlite3 db/dn_jmx.db  "${hourly_tbl_ddl}"
   echo "\nDDL from Show table :"
-  sqlite3 db/dn_jmx.db  ".schema ${dn_hourly_table}"
+  sqlite3 db/dn_jmx.db  ".schema dn_hourly"
   #connect DB and import report file into daily table
 
   for hourly_file in ${hourly_report_dir}/${date_id}/*;do
-    echo ".separator ","\n.import ${hourly_file} ${dn_hourly_table}" | sqlite3 db/dn_jmx.db
+    echo ".separator ","\n.import ${hourly_file} dn_hourly" | sqlite3 db/dn_jmx.db
     echo "** file  ##  ${hourly_file} ## is loaded "
   done
 
-  echo "\n # Number of rows on ${dn_hourly_table}  :\n"
-  sqlite3 db/dn_jmx.db  "select count(*) from ${dn_hourly_table}"
+  echo "\n # Number of rows on dn_hourly  :\n"
+  sqlite3 db/dn_jmx.db  "select count(*) from dn_hourly"
 
 }
 
-load_daily_table(){
+prep_daily_file(){
 
-  sqlite3 db/dn_jmx.db  "create table dn_daily as select * from  ${dn_hourly_table} where 0"
+  # Aggregate across all hourly data and make one daily file and place under report/daily/
+
+  sqlite3 db/dn_jmx.db  "create table dn_daily as select * from  dn_hourly where 0"
   echo "\nDDL from Show dn_daily :"
   sqlite3 db/dn_jmx.db  ".schema dn_daily"
   #connect DB and import report file into daily table
-  sqlite3 db/dn_jmx.db "select * from ${dn_hourly_table} where HostName=\"cent7-hdp-1.field.hortonworks.com\";"
 
     aggregate_hourly_table_query="insert into dn_daily select HostName,substr(poll_id,9,10) as date_id"
     for col in ${dn_metrics_list};do
       aggregate_hourly_table_query="${aggregate_hourly_table_query} ,avg(${col})"
     done
-    aggregate_hourly_table_query="${aggregate_hourly_table_query} from ${dn_hourly_table} group by HostName,substr(poll_id,9,10);"
+    aggregate_hourly_table_query="${aggregate_hourly_table_query} from dn_hourly group by HostName,substr(poll_id,9,10);"
 
   echo " agg query is \n: ${aggregate_hourly_table_query}"
   sqlite3 db/dn_jmx.db  "${aggregate_hourly_table_query}"
 
   echo "\nSelect table dn_daily troubleshoot purpose only :\n"
-
   echo ".mode csv\n.output ${dn_daily_agg_file}\nSelect * from dn_daily;\n.quit" | sqlite3 db/dn_jmx.db
 
   sqlite3 db/dn_jmx.db  "select * from dn_daily"
   sqlite3 db/dn_jmx.db  "delete from dn_daily"
 
 }
+
+prep_weekly_file(){
+
+  # Read all files under report/daily/* populate dn_daily --> aggregate and load into dn_weekly and
+  # upload it to report/weekly/
+
+  echo "\nLET US START\n"
+  for daily_file in report/daily/*;do
+    echo ".separator ","\n.import ${daily_file} dn_daily" | sqlite3 db/dn_jmx.db
+    echo "** file  ##  ${daily_file} ## is loaded "
+  done
+  sqlite3 db/dn_jmx.db  "select * from  dn_daily"
+  echo "\nLET US END\n"
+
+
+  sqlite3 db/dn_jmx.db  "create table dn_weekly as select * from  dn_daily where 0"
+  echo "\nDDL from Show dn_weekly :"
+  sqlite3 db/dn_jmx.db  ".schema dn_weekly"
+  #connect DB and import report file into daily table
+
+    aggregate_weekly_table_query="insert into dn_weekly select HostName,substr(poll_id,1,7) as date_id"
+    for col in ${dn_metrics_list};do
+      aggregate_weekly_table_query="${aggregate_weekly_table_query} ,avg(${col})"
+    done
+    aggregate_weekly_table_query="${aggregate_weekly_table_query} from dn_daily group by HostName,substr(poll_id,1,7);"
+
+  echo " agg query is \n: ${aggregate_weekly_table_query}"
+  sqlite3 db/dn_jmx.db  "${aggregate_weekly_table_query}"
+  echo ".mode csv\n.output ${dn_weekly_agg_file}\nSelect * from dn_weekly;\n.quit" | sqlite3 db/dn_jmx.db
+
+  echo "\nSelect table dn_weekly troubleshoot purpose only :\n"
+  sqlite3 db/dn_jmx.db  "select * from dn_weekly"
+  sqlite3 db/dn_jmx.db  "delete from dn_weekly"
+
+}
+
 ########################################## DN ends ####################################################################
 #work on DNs
 #work on NMs
@@ -157,7 +204,8 @@ clean_up(){
 
 prep_dn_hourly_table
 poll_hourly_dn_jmx
-load_hourly_table
-load_daily_table
+prep_hourly_file
+prep_daily_file
+prep_weekly_file
 
 clean_up
