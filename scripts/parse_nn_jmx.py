ecaecbc5ae19e1f64a80d58a4a89d3543c0699fe
knatarasan <kannappan_natarasan@yahoo.com>
2019-05-14 11:34:20 -0700

    parse_nn_jmx.py is good to read a json file or extract json out of URL- working good

---
+++ scripts/parse_nn_jmx.py
@@ -0,0 +1,32 @@
+import json
+import urllib2
+url = "http://cent7-hdp-1.field.hortonworks.com:50070/jmx"
+
+
+def readJsonFile(filename):
+    with open(filename) as json_file:
+        jsonDict = json.load(json_file)
+        return jsonDict
+
+
+def readJsonURL(url):
+    response = urllib2.urlopen(url)
+    nnjmx = response.read()
+    jsonDict = json.loads(nnjmx)
+    return jsonDict
+
+# data=readJsonFile('NN2.json')
+data=readJsonURL(url)
+
+li=data['beans']
+
+li2=[]
+for i in li:
+    if( 'SlowPeersReport' in i.keys()):
+        if isinstance(i.get('SlowPeersReport') , unicode ):
+            li2=eval(i.get('SlowPeersReport'))
+        elif isinstance(i.get('SlowPeersReport') , list ):
+            li2=i.get('SlowPeersReport')
+
+for i in li2:
+    print(i.get('SlowNode'))
