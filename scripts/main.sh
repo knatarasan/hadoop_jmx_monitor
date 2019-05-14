96863342cb1933abdc0917c64756b7b801023b4e
knatarasan <kannappan_natarasan@yahoo.com>
2019-05-14 15:28:54 -0700

    main.sh had been parameterized for dn and nm

--- scripts/main.sh
+++ scripts/main.sh
@@ -24,15 +24,16 @@ touch ${log_file} ${missing_hosts} ${sql_log}
 
 
 #Send hourly files into folder with name as date
-*****
-# dn_hourly_report_file=${hourly_report_dir}/${date_id}/dn_report_${poll_id}.csv
-dn_hourly_report_file=${hourly_report_dir}/${date_id}/dn_report_${poll_id}.csv
 
-dn_daily_agg_file=report/daily/dn_daily_${date_id}.csv
-dn_weekly_agg_file=report/weekly/dn_weekly_${month_id}.csv
-# dn_hourly_report_file=${hourly_report_dir}/dn_report_poll_id_2019_05_06_16_49_11.csv
+prep_files(){
+  hourly_report_file=${hourly_report_dir}/${date_id}/${node}_report_${poll_id}.csv
+  daily_agg_file=report/daily/${node}_daily_${date_id}.csv
+  weekly_agg_file=report/weekly/${node}_weekly_${month_id}.csv
 
+  metrics_list=`cat config/${node}_metrics_config`
+  column_list="HostName poll_id "
 
+}
 
 
 logg(){
@@ -50,36 +51,38 @@ elif [ ! -f config/nm_hosts ]
 then
   logg  "check for file config/nm_hosts with required host names in it "
   exit 1
+elif [ ! -f config/dn_metrics_config ]
+then
+  logg  "check for file config/dn_metrics_config with required host names in it "
+  exit 1
+elif [ ! -f config/nm_metrics_config ]
+then
+  logg  "check for file config/nm_metrics_config with required host names in it "
+  exit 1
+fi
 
 
-##############################################  DN ######################################################
-*****
-dn_metrics_list=`cat config/dn_metrics_config`
-dn_column_list="HostName poll_id "
 
-*****
-prep_dn_hourly_table(){
+prep_hourly_table(){
 
-  logg "prep_dn_hourly_table would be executed"
+  logg "prep_${node}_hourly_table would be executed"
   # This hourly table is ddl is derived from metrics list provided from config file
 
   #derive column names from config file
-  for i in $dn_metrics_list;do
-    dn_column_list="${dn_column_list} ${i}"
+  for i in $metrics_list;do
+    column_list="${column_list} ${i}"
   done
 
-  dn_column_arr=($dn_column_list)
-
-
-  dn_hourly_table=dn_hourly
+  column_arr=($column_list)
+  # hourly_table=${node}_hourly
 
   #Prepare DDL of table
-  hourly_tbl_ddl="create table dn_hourly ("
+  hourly_tbl_ddl="create table ${node}_hourly ("
 
-  length=${#dn_column_arr[@]}
+  length=${#column_arr[@]}
   current=0
 
-  for col in "${dn_column_arr[@]}"; do
+  for col in "${column_arr[@]}"; do
     current=$((current + 1))
 
     if [[ "$current" -eq 1 ]]; then
@@ -94,22 +97,23 @@ prep_dn_hourly_table(){
   done
 
   hourly_tbl_ddl="${hourly_tbl_ddl} );"
+
 }
 
 
-*****
-poll_hourly_dn_jmx(){
 
-  #Hourly files is prepared by polling all DNs for every 30 min interval, metrics collected would be
+poll_hourly_jmx(){
+
+  #Hourly files is prepared by polling all nodes for every 30 min interval, metrics collected would be
   #stored under report/hourly/yyyy_mm_dd
 
-  logg  "Poll hourly table "
+  logg  "Poll hourly table ${node}"
   #initialize row values
   row=""
 
   #traverse through all slave nodes supplied from File
   while read host_name; do
-   curl http://${host_name}:50075/jmx>tmp_${host_name}
+   curl http://${host_name}:${node_port}/jmx>tmp_${host_name}
 
    if [ $? -ne 0 ]
    then
@@ -123,13 +127,13 @@ poll_hourly_dn_jmx(){
    row="${host_name},${poll_id},"
 
    #grep and populate metrics values , metrics names provided from config file
-   for i in ${dn_metrics_list};do
+   for i in ${metrics_list};do
      #"head -1" has been added since metric "NumFailedVolumes" had more than one value
      row="$row"`grep \"${i}\" tmp_${host_name}|head -1|cut -f2 -d ':'`
    done
    row=${row%?}
 
-   echo $row>>${dn_hourly_report_file}
+   echo $row>>${hourly_report_file}
 
    row=""
   rm tmp_${host_name}
@@ -137,29 +141,28 @@ poll_hourly_dn_jmx(){
 
 }
 
-*****
+
 prep_hourly_file(){
 
-  # read all hourly files under report/hourly/yyyy_mm_dd which was pulled by poll_hourly_dn_jmx
+  # read all hourly files under report/hourly/yyyy_mm_dd which was pulled by poll_hourly_jmx
   # and load into dn_hourly table
-
 
   #connect DB create tabel
   sqlite3 db/dn_jmx.db  "${hourly_tbl_ddl}" >>${sql_log}
 
-  sqlite3 db/dn_jmx.db  ".schema dn_hourly" >>${sql_log}
+  sqlite3 db/dn_jmx.db  ".schema ${node}_hourly" >>${sql_log}
   #connect DB and import report file into daily table
 
 
 
-  for hourly_file in ${hourly_report_dir}/${date_id}/*;do
-    echo  ".separator ","\n.import ${hourly_file} dn_hourly" | sqlite3 db/dn_jmx.db
+  for hourly_file in ${hourly_report_dir}/${date_id}/${node}*;do
+    echo  ".separator ","\n.import ${hourly_file} ${node}_hourly" | sqlite3 db/dn_jmx.db
     logg  "** file  ##  ${hourly_file} ## is loaded "
   done
 
 
-  echo  " Number of rows on dn_hourly  : " >>${sql_log}
-  sqlite3 db/dn_jmx.db  "select count(*) from dn_hourly" >>${sql_log}
+  echo  " Number of rows on ${node}_hourly  : " >>${sql_log}
+  sqlite3 db/dn_jmx.db  "select count(*) from ${node}_hourly" >>${sql_log}
 
 }
 
@@ -167,21 +170,21 @@ prep_daily_file(){
 
   # Aggregate across all hourly data and make one daily file and place under report/daily/
 
-  sqlite3 db/dn_jmx.db  "create table dn_daily as select * from  dn_hourly where 0"
+  sqlite3 db/dn_jmx.db  "create table ${node}_daily as select * from  ${node}_hourly where 0"
 
-  sqlite3 db/dn_jmx.db  ".schema dn_daily"   >>${sql_log}
+  sqlite3 db/dn_jmx.db  ".schema ${node}_daily"   >>${sql_log}
   #connect DB and import report file into daily table
 
-    aggregate_hourly_table_query="insert into dn_daily select HostName,substr(poll_id,9,10) as date_id"
-    for col in ${dn_metrics_list};do
+    aggregate_hourly_table_query="insert into ${node}_daily select HostName,substr(poll_id,9,10) as date_id"
+    for col in ${metrics_list};do
       aggregate_hourly_table_query="${aggregate_hourly_table_query} ,avg(${col})"
     done
-    aggregate_hourly_table_query="${aggregate_hourly_table_query} from dn_hourly group by HostName,substr(poll_id,9,10);"
+    aggregate_hourly_table_query="${aggregate_hourly_table_query} from ${node}_hourly group by HostName,substr(poll_id,9,10);"
 
-  echo  "Insert into dn_daily \n: ${aggregate_hourly_table_query} \n" >>${sql_log}
+  echo  "Insert into ${node}_daily Query : \n ${aggregate_hourly_table_query} \n" >>${sql_log}
   sqlite3 db/dn_jmx.db  "${aggregate_hourly_table_query}"
 
-  echo  ".mode csv\n.output ${dn_daily_agg_file}\nSelect * from dn_daily;\n.quit" | sqlite3 db/dn_jmx.db
+  echo  ".mode csv\n.output ${daily_agg_file}\nSelect * from ${node}_daily;\n.quit" | sqlite3 db/dn_jmx.db
 
   # sqlite3 db/dn_jmx.db  "delete from dn_daily"
 
@@ -191,26 +194,26 @@ prep_weekly_file(){
   # Read all files under report/daily/* populate dn_daily --> aggregate and load into dn_weekly and
   # upload it to report/weekly/
 
-  for daily_file in report/daily/*;do
-    echo  ".separator ","\n.import ${daily_file} dn_daily" | sqlite3 db/dn_jmx.db
+  for daily_file in report/daily/${node}*;do
+    echo  ".separator ","\n.import ${daily_file} ${node}_daily" | sqlite3 db/dn_jmx.db
     logg  "** file  ##  ${daily_file} ## is loaded "
   done
 
 
-  sqlite3 db/dn_jmx.db  "create table dn_weekly as select * from  dn_daily where 0"
+  sqlite3 db/dn_jmx.db  "create table ${node}_weekly as select * from  ${node}_daily where 0"
 
-  sqlite3 db/dn_jmx.db  ".schema dn_weekly" >> ${sql_log}
+  sqlite3 db/dn_jmx.db  ".schema ${node}_weekly" >> ${sql_log}
   #connect DB and import report file into daily table
 
-    aggregate_weekly_table_query="insert into dn_weekly select HostName,substr(poll_id,1,7) as date_id"
-    for col in ${dn_metrics_list};do
+    aggregate_weekly_table_query="insert into ${node}_weekly select HostName,substr(poll_id,1,7) as date_id"
+    for col in ${metrics_list};do
       aggregate_weekly_table_query="${aggregate_weekly_table_query} ,avg(${col})"
     done
-    aggregate_weekly_table_query="${aggregate_weekly_table_query} from dn_daily group by HostName,substr(poll_id,1,7);"
+    aggregate_weekly_table_query="${aggregate_weekly_table_query} from ${node}_daily group by HostName,substr(poll_id,1,7);"
 
-  echo  "Insert into dn_weekly \n: ${aggregate_weekly_table_query}" >>${sql_log}
+  echo  "Insert into ${node}_weekly \n: ${aggregate_weekly_table_query}" >>${sql_log}
   sqlite3 db/dn_jmx.db  "${aggregate_weekly_table_query}"
-  echo  ".mode csv\n.output ${dn_weekly_agg_file}\nSelect * from dn_weekly;\n.quit" | sqlite3 db/dn_jmx.db
+  echo  ".mode csv\n.output ${weekly_agg_file}\nSelect * from ${node}_weekly;\n.quit" | sqlite3 db/dn_jmx.db
 
 
   # sqlite3 db/dn_jmx.db  "select * from dn_weekly" >>${log_file}
@@ -225,14 +228,14 @@ prep_weekly_file(){
 health_check(){
   # Read current hourly file , compare with weekly avg , if found +/- 30% of weekly average return health red
 
-  sqlite3 db/dn_jmx.db  "create table dn_current as select * from  dn_hourly where 0"
-  echo  ".separator ","\n.import ${dn_hourly_report_file} dn_current" | sqlite3 db/dn_jmx.db
+  sqlite3 db/dn_jmx.db  "create table ${node}_current as select * from  ${node}_hourly where 0"
+  echo  ".separator ","\n.import ${hourly_report_file} ${node}_current" | sqlite3 db/dn_jmx.db
 
       health_check_query="select cur.HostName"
-      for col in ${dn_metrics_list};do
+      for col in ${metrics_list};do
         health_check_query="${health_check_query} ,case when ( (cur.${col}-week.${col})/cur.${col} ) >0.3 then \"${col} :1 ***\" else \"${col} :0\" end"
       done
-      health_check_query="${health_check_query} from dn_current cur inner join dn_weekly week  where cur.HostName=week.HostName;"
+      health_check_query="${health_check_query} from ${node}_current cur inner join ${node}_weekly week  where cur.HostName=week.HostName;"
 
   echo  "\nhealth check query : \n ${health_check_query}"  >>${sql_log}
   sqlite3 db/dn_jmx.db  "${health_check_query}"  >>${sql_log}
@@ -255,12 +258,26 @@ check_nn(){
   /usr/bin/python2.7 scripts/parse_nn_jmx.py
 }
 
-check_nn
-# prep_dn_hourly_table
-# poll_hourly_dn_jmx
-# prep_hourly_file
-# prep_daily_file
-# prep_weekly_file
-# health_check
-# # verbo
+exec_run(){
+  echo "Run starts for : ${node}"
+  check_nn
+  prep_files
+  prep_hourly_table
+  poll_hourly_jmx
+  prep_hourly_file
+  prep_daily_file
+  prep_weekly_file
+  health_check
+}
+
+# node=dn
+# node_port=50075
+# exec_run
+
+node=nm
+node_port=8042
+exec_run
+
+
+verbo
 # clean_up
