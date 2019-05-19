f4cf67a87ca26e4bc7313bfcce922a5d4e0f3b85
knatarasan <kannappan_natarasan@yahoo.com>
2019-05-19 16:47:43 -0700

    parse_dn_jmx parses following files 	dn_metrics_config.json 	dn0064.json
    and prepares the following
    	DDL struct
    	value extracted from dn0064.json

--- scripts/parse_dn_jmx.py
+++ scripts/parse_dn_jmx.py
@@ -2,7 +2,6 @@ import json
 import urllib2
 import sys
 
-
 def readJsonFile(filename):
     with open(filename) as json_file:
         jsonDict = json.load(json_file)
@@ -21,27 +20,55 @@ config=readJsonFile('config/dn_metrics_config.json')    # config json for column
 
 # read metrics --> get keys --> check headertype
 
+features=jmx_data['beans']
+
+# def getJmxData(config_headertype,config_key,config_columns):
+    # print 'getJmxData',config_headertype,config_key,config_columns
+
+
+
+
+
 def getDDLStruct():
     config_metrics=config['metrics']
 
+    print 'to process partial columns - volume columns'
     for config_key in config_metrics:
         config_headertype=config_metrics.get(config_key)[0].get('headertype')
         config_columns=config_metrics.get(config_key)[1]
-        # print config_key,':',config_headertype
-        for column,dtype in config_columns.iteritems():
-            # print column,':',dtype
+        # getJmxData(config_headertype,config_key,config_columns)
 
-            jmx_key=''
-            if(config_headertype=='partial'):
-                for i in jmx_data['beans']:
-                    if(i.get('name').find(config_key)==0):      # eg => i.get('name') : 'Hadoop:service=DataNode,name=DataNodeActivity-phxhdc19dn0064.phx.paypalinc.com-1019'
-                        jmx_key=i.get('name')
-                        # print 'jmx_key : ',jmx_key,' config_columns :',config_columns
-                        print jmx_key,':',column,' : ',i.get(column)
+        table_columns_ddl=[]
+        table_columns_values=[]
+        # to process only partial columns
+        if(config_headertype=='partial'):
+            for jmx_feature in features:
+                if(jmx_feature.get('name').find(config_key)==0):
+                    jmx_key=jmx_feature.get('name')
+
+                    for volume_feature_name,volume_feature_dtype in config_columns.iteritems():
+                        if( jmx_key.find('Hadoop:service=DataNode,name=DataNodeVolume-') ==0):
+
+                            #Prepare column name part
+                            disknum=jmx_key.find('/')
+                            volumename=jmx_key[disknum+1:disknum+9]
+                            sq_columnname=volumename+'_'+ volume_feature_name+' '+volume_feature_dtype
+                            table_columns_ddl.append(sq_columnname)
+
+                            #Prepare column value part
+                            volume_feature_value={ volumename+'_'+ volume_feature_name : jmx_feature.get( volume_feature_name) }
+                            table_columns_values.append(volume_feature_value)
+
+                            # print 'jmx_key :',jmx_key,' k :',k,' value_from_jmx: ',i.get(k)
+
+        else:
+            print 'to process non partial columns'
 
 
+    print table_columns_ddl
+    print table_columns_values
 
-    print '    --- '
+
 
 print getDDLStruct()
 
